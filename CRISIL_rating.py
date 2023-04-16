625637b8ebfc1534b81d40ab403388dd75f82c5b
Snigdha Srivastava <106507659+Shubhi-73@users.noreply.github.com>
2023-04-16 16:00:10 +0530

    Applying logistic regression sklearn functions

--- CRISIL_rating.py
+++ CRISIL_rating.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 import sklearn as sk
 from sklearn.linear_model import LogisticRegression
@@ -14,12 +15,31 @@ filepath = r"Downloads\corporate_rating.csv"
 df = pd.read_csv(filepath)
 
 # selecting all coulmns from currentRatio Onwards
-x = df.iloc[:, 6:31]
-y = df["Rating"]
-
+x = df.iloc[:2025, 6:31]
+y = df.iloc[:2025, 0]
+x_test = df.iloc[2030:, 6:31]
 # print the first five rows
-print(x.head())
+# x.head()
+
 print(y.head())
 
-# model = LogisticRegression(solver='lbfgs', multi_class='multinomial',random_state=0)
-# model.fit(x, y)
+print(x["daysOfSalesOutstanding"])
+
+# scale with feature names
+
+
+scaler = StandardScaler()
+scaler.fit(x)
+x = pd.DataFrame(scaler.transform(x), columns=x.columns)
+x_test = pd.DataFrame(scaler.transform(x_test), columns=x.columns)
+
+print(x["daysOfSalesOutstanding"])
+
+model = LogisticRegression(
+    solver="newton-cg", multi_class="multinomial", random_state=0, max_iter=1000
+)
+model.fit(x, y)
+
+y_pred = model.predict(x_test)
+print(df.iloc[2030:, 6:31])
+print(y_pred)
