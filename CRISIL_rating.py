81097cb4a63763dba6663786341d6d45ede23a04
Snigdha Srivastava <106507659+Shubhi-73@users.noreply.github.com>
2023-04-20 21:38:10 +0530

    selected features functions

--- CRISIL_rating.py
+++ CRISIL_rating.py
@@ -4,6 +4,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import sklearn as sk
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.datasets import make_classification
+from sklearn.feature_selection import RFECV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 
 import pandas as pd
@@ -25,7 +31,15 @@ print(y.head())
 
 print(x.head())
 
+
+# selector = SelectKBest(chi2, k=10)
+# x = selector.fit_transform(x, y)
+# x_test = selector.transform(x_test)
+
+
 # scale with feature names
+
+x, y, x_test, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
 
 
 scaler = StandardScaler()
@@ -36,20 +50,30 @@ print("These are x values")
 
 print(x)
 
-pca = PCA(n_components=12)
-x = pca.fit_transform(x)
-x_test = pca.transform(x_test)
-
-explained_variance = pca.explained_variance_ratio_
-print(explained_variance)
+# pca = PCA(n_components=12)
+# x = pca.fit_transform(x)
+# x_test = pca.transform(x_test)
 
 
-model = LogisticRegression(
-    solver="newton-cg", multi_class="multinomial", random_state=0, max_iter=1000
-)
-model.fit(x, y)
+estimator = DecisionTreeClassifier()
 
-y_pred = model.predict(x_test)
-print("THIS IS X TEST")
-print(x_test)
+# Create an RFECV object
+selector = RFECV(estimator, cv=2)
+
+# Fit the RFECV object to the data
+selector.fit(x, y)
+
+# Print the selected features
+selected_features = selector.transform(x)
+
+clf = LogisticRegression(multi_class="multinomial", solver="lbfgs", max_iter=1000)
+clf.fit(selected_features, y)
+
+
+# scores = cross_val_score(clf, x, y, cv=3)
+# print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+selected_features_test = selector.transform(x_test)
+
+y_pred = clf.predict(selected_features_test)
 print(y_pred)
